fca49c9171d70b2f47cb4ae35e1f2a6066d4e447
serik <bukenoff.s@gmail.com>
2019-07-22 20:36:27 +0700

    clean up code

--- src/redux/sagas.ts
+++ src/redux/sagas.ts
@@ -29,51 +29,6 @@ function* openCellRecursive(row, col) {
     }
   }
 
-
-  // if(this_cell.bombs_around === 0){
-  //   for (let i = 0; i < this_cell.neighbours.length; i++) {
-  //     const { row, col } = this_cell.neighbours[i];
-  //     const neighbour_cell = mines[row][col];
-  //     console.log(neighbour_cell);
-  //     // recursion should happen only if cell is empty
-  //     if (neighbour_cell.bombs_around === 0 && !neighbour_cell.open) {
-  //       //console.log('recursion');
-  //       console.log(row, col);
-  //       yield call(openCellRecursive, this_cell.neighbours[i].row, this_cell.neighbours[i].col);
-  //     } else {
-  //       //console.log('no recursion');
-  //     }
-  //   }
-  // }
-
-  // iterate through neighbours of this cell and open them
-  // for (let i = 0; i < this_cell.neighbours.length; i++) {
-  //   const { row, col } = this_cell.neighbours[i];
-  //   const neighbour_cell = mines[row][col];
-
-  //   // if it's not open already open it now
-  //   if (!neighbour_cell.open) {
-  //     yield put(actionCreators.setCellOpen(row, col));
-  //   }
-  // }
-
-  // iterate through neighbours again
-  // detect empty cells
-  // call saga recursively once such a cell is detected
-  // for (let i = 0; i < this_cell.neighbours.length; i++) {
-  //   const { row, col } = this_cell.neighbours[i];
-  //   const neighbour_cell = mines[row][col];
-  //   console.log(neighbour_cell);
-  //   // recursion should happen only if cell is empty
-  //   if (neighbour_cell.bombs_around === 0 && !neighbour_cell.open) {
-  //     //console.log('recursion');
-  //     console.log(row, col);
-  //     yield call(openCellRecursive, this_cell.neighbours[i].row, this_cell.neighbours[i].col);
-  //   } else {
-  //     //console.log('no recursion');
-  //   }
-  // }
-
   console.log('recursive saga ends');
 }
 
@@ -88,12 +43,8 @@ function* openCellSaga({ row, col }: ReturnType<typeof actionCreators.openCell>)
     return;
   }
 
-  //yield put(actionCreators.setCellOpen(row, col));
   yield call(openCellRecursive, row, col);
   // start recursion
-  // if (this_cell.bombs_around === 0) {
-  //   yield call(openCellRecursive, row, col);
-  // }
 }
 
 function* flow() {
