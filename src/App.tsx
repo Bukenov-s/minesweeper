7bc975c919a73880d83b3eac7143103bc9534803
serik <sbukenov@allmax.team>
2019-08-21 08:59:56 +0700

    prettified imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { hot } from 'react-hot-loader/root';
 import React, { FC, Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import { getDifficulty, getGameOver, getResult, getBombsCounter, getTimer, getMines, getBombs } from './redux/selectors';
+import * as selectors from './redux/selectors';
 import Table from '~/containers/Table';
 import Panel from '~/containers/Panel';
 import Menu from '~/containers/Menu';
 import './style.css';
 
 const App: FC<any> = ({ startNewGame }) => {
-  const difficulty = useSelector(getDifficulty);
-  const result = useSelector(getResult);
-  const game_over = useSelector(getGameOver);
-  const bombs_counter = useSelector(getBombsCounter);
-  const timer = useSelector(getTimer);
-  const mines = useSelector(getMines);
-  const bombs = useSelector(getBombs);
+  const difficulty = useSelector(selectors.getDifficulty);
+  const result = useSelector(selectors.getResult);
+  const game_over = useSelector(selectors.getGameOver);
+  const bombs_counter = useSelector(selectors.getBombsCounter);
+  const timer = useSelector(selectors.getTimer);
+  const mines = useSelector(selectors.getMines);
+  const bombs = useSelector(selectors.getBombs);
 
   return (
     <div className="app">
